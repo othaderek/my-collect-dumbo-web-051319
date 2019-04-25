58586302c2759495df2d2a27fae882813fd446f4
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-25 20:22:32 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
+def my_collect(arr)
+  i = 0 
+  new_arr = []
+  while i < arr.length 
+    new_arr << yield(arr[i])
+    i += 1 
+  end
+  new_arr
+end
 
+students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
+languages = ['ruby', 'javascript', 'python', 'objective-c']
+
+my_collect(students) do |name|
+  name.split(" ").first
+end
+
+my_collect(languages) do |name|
+  name.upcase
+end
 
