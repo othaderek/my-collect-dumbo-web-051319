4e2b3db974377ccc59ef9b32bd4cdccc323c8098
Otha D Hernandez <otha.hernandez@gmail.com>
2019-05-06 23:33:34 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(arr)
   i = 0 
   new_arr = []
   while i < arr.length 
-    new_arr << yield(arr[i])
+    new_arr << yield(arr[i]) # Saves yielded return value to arr
     i += 1 
   end
   new_arr
